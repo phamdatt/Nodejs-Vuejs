3a662c0d44d5010d0a7a41945379a9712cee8dfe
phamdat <tiendatt3009@gmail.com>
2022-11-03 18:17:23 +0700

    update component header and footer

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,25 +1,15 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HomePage />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HomePage from "./page/HomePage.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HomePage,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ my-app/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Header</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
